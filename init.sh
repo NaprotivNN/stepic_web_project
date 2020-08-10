dc9c625e9a073d519782c217dc9487b73ba262f0
NaprotivNN <makir.mail@gmail.com>
2020-08-10 11:07:44 +0300

    test

--- init.sh
+++ init.sh
@@ -1,14 +1,17 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/gunicorn restart
 
-#sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
 
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
+sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
+
+
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+#sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
 
 #sudo gunicorn -c hello.py hello:wsgi_app
 
