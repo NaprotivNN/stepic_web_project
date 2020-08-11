cb753aad75b8bb4d724d2c4e93a389c49cca9d4a
NaprotivNN <makir.mail@gmail.com>
2020-08-11 22:28:36 +0300

    OK

--- init.sh
+++ init.sh
@@ -3,30 +3,9 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-
-#sudo /etc/init.d/gunicorn restart
-
-#sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
-
-#sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
-
-
-#sudo rm /etc/nginx/sites-enabled/default
-
-#sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 
-
-
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
 sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
-
-#sudo gunicorn -c hello.py hello:wsgi_app
-
-#вместо 2х верхних строчек можно написать и так,но тогда в hello.py нужно добавить
-#bind='0.0.0.0:8080'
-
