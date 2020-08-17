2feee7b597874b593e3ae78854cb27feea522760
NaprotivNN <makir.mail@gmail.com>
2020-08-17 21:14:43 +0300

    add stepic.bd

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'qa',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stepic',
+        'USER': 'root',
+        'PASSWORD':'privetSQL',
     }
 }
 
